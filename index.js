dc3699e8ea4febe317e0b9c12e1ef73c5d6d3557
Olga Nadongo <winnienadongo@gmail.com>
2022-06-07 15:09:00 +0300

    completed labs

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let cost;
+  if(feet <= 400){
+    cost="This one is on me!"
+  }else if(feet>2000 && feet<=2500){
+    cost="I will gladly take your thirty bucks."
+  } else if(feet>2500){
+    cost="No can do."
+  }
+  return cost
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const cityMessage=
+  city==='NYC'? "Ok, sounds good.": "No go.";
+  return cityMessage;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+   let tipMessage;
+   if(tip==="generous"){
+     tipMessage="Thank you so much."
+   }else if(tip==="not as generous"){
+     tipMessage="Thank you."
+   }
+   else{
+     tipMessage="Bye."
+   }
+   return tipMessage
+
 }
